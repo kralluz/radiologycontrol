2016add8ff8ddbd1b90e7436e5e3eb92aa3b66c4
Carlos Henrique klz <chenrique201520@gmail.com>
2024-06-14 15:17:57 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// App.js
 import React, { useState } from "react";
 import "./App.css";
 import { jsPDF } from "jspdf";
@@ -10,7 +9,7 @@ import ConfirmDeleteModal from "./components/ConfirmDeleteModal";
 
 function App() {
   const [records, setRecords] = useState([]);
-  const [pdfResults, setPdfResults] = useState("");
+  const [pdfResults, setPdfResults] = useState(""); // Este estado armazena os resultados da análise do PDF
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [indexToDelete, setIndexToDelete] = useState(null);
 
@@ -24,8 +23,8 @@ function App() {
   };
 
   const closeModal = () => {
-    setIndexToDelete(null);
     setIsModalOpen(false);
+    setIndexToDelete(null);
   };
 
   const confirmDelete = () => {
@@ -40,18 +39,20 @@ function App() {
     const marginLeft = 10;
     let yOffset = 10;
 
-    // Título
     doc.setFontSize(16);
-    doc.text("RadiologyControl - Relatório de Procedimentos", marginLeft, yOffset);
+    doc.text(
+      "RadiologyControl - Relatório de Procedimentos",
+      marginLeft,
+      yOffset
+    );
     yOffset += 15;
 
-    // Resultados da Análise de PDF
     doc.setFontSize(14);
-    doc.text("Resultados da Análise de PDF:", marginLeft, yOffset);
+    doc.text("Relatório de exames:", marginLeft, yOffset);
     yOffset += 10;
 
     doc.setFontSize(12);
-    pdfResults.split('\n').forEach(line => {
+    pdfResults.split("\n").forEach((line) => {
       doc.text(line, marginLeft, yOffset);
       yOffset += 7;
     });
@@ -64,22 +65,24 @@ function App() {
       doc.text("Registros Cirúrgicos:", marginLeft, yOffset);
       yOffset += 10;
 
-      const tableData = records.map(record => [
-        `${record.date} ${record.time}`, // Combina data e hora
+      const tableData = records.map((record) => [
+        `${record.date} ${record.time}`,
         record.timeSpent,
         record.doctorName,
         record.radiologistName,
-        record.procedureName
+        record.procedureName,
       ]);
 
       doc.autoTable({
         startY: yOffset,
-        head: [['Data e Hora', 'Horas', 'Médico', 'Radiologista', 'Procedimento']],
+        head: [
+          ["Data e Hora", "Horas", "Médico", "Radiologista", "Procedimento"],
+        ],
         body: tableData,
         margin: { top: 10, left: 10, right: 10 },
-        styles: { cellPadding: 3, fontSize: 12, halign: 'center' },
+        styles: { cellPadding: 3, fontSize: 12, halign: "center" },
         headStyles: { fillColor: [0, 77, 153] }, // Cor azul similar ao header
-        theme: 'grid' // Estilo de tabela
+        theme: "grid",
       });
 
       yOffset = doc.lastAutoTable.finalY + 10; // Atualiza o yOffset para o final da tabela
@@ -93,24 +96,20 @@ function App() {
       <Header />
       <PdfAnalysis setPdfResults={setPdfResults} />
       <GetDataRadiologySupport addRecord={addRecord} />
-      <button
-        onClick={generatePdfDocument}
-        className="generate-pdf-button" /* Use a classe CSS aqui */
-      >
+      <button onClick={generatePdfDocument} className="generate-pdf-button">
         Gerar PDF dos Dados
       </button>
       <div className="results">
         {records.map((record, index) => (
           <div key={index} className="record">
-            <button
-              className="remove-button"
-              onClick={() => openModal(index)}
-            >
+            <button className="remove-button" onClick={() => openModal(index)}>
               X
             </button>
             <div>
               <label>Data e Hora:</label>
-              <span>{record.date} {record.time}</span>
+              <span>
+                {record.date} {record.time}
+              </span>
             </div>
             <div>
               <label>Horas gastas:</label>
--- src/components/PdfAnalysis.jsx
+++ src/components/PdfAnalysis.jsx
@@ -1,54 +1,91 @@
-import React, { useState } from 'react';
-import './PdfAnalysis.css'; // Arquivo de estilos para PdfAnalysis
+import React, { useState } from "react";
+import "./PdfAnalysis.css"; // Arquivo de estilos para PdfAnalysis
 
 function PdfAnalysis({ setPdfResults }) {
   const [analysisResults, setAnalysisResults] = useState("");
+  const [tcCount, setTcCount] = useState(0); // Estado para contar as ocorrências de 'TC'
+  const [rxCount, setRxCount] = useState(0); // Estado para contar as ocorrências de 'RX'
+  const [lombarFemurCount, setLombarFemurCount] = useState(0); // Conta 'do coluna lombar e do fêmur'
+  const [corpoInteiroCount, setCorpoInteiroCount] = useState(0); // Conta 'do corpo inteiro'
 
   const processPDF = (file) => {
     const fileReader = new FileReader();
-    fileReader.onload = function() {
+    fileReader.onload = function () {
       const typedArray = new Uint8Array(this.result);
-      const loadingTask = window['pdfjsLib'].getDocument(typedArray);
-      loadingTask.promise.then(pdf => {
-        pdf.getPage(1).then(page => {
-          page.getTextContent().then(textContent => {
-            const text = textContent.items.map(item => item.str).join(' ');
-            analyzeText(text);
+      const loadingTask = window["pdfjsLib"].getDocument(typedArray);
+      loadingTask.promise.then(
+        (pdf) => {
+          const numPages = pdf.numPages;
+          const pagePromises = [];
+
+          for (let pageNum = 1; pageNum <= numPages; pageNum++) {
+            pagePromises.push(
+              pdf.getPage(pageNum).then((page) => {
+                return page.getTextContent().then((textContent) => {
+                  const text = textContent.items
+                    .map((item) => item.str)
+                    .join(" ");
+                  return text; // Retorna o texto para futura agregação
+                });
+              })
+            );
+          }
+
+          Promise.all(pagePromises).then((pagesTexts) => {
+            const fullText = pagesTexts.join(" ");
+            setAnalysisResults(fullText); // Atualiza o estado com os resultados da análise de todas as páginas
+            const tcTotalCount = (
+              fullText.match(/\bTC\b(?=[ ,.;?!]|$)/gi) || []
+            ).length;
+            const rxTotalCount = (
+              fullText.match(/\bRX\b(?=[ ,.;?!]|$)/gi) || []
+            ).length;
+            const lombarFemurTotalCount = (
+              fullText.match(/do coluna lombar e do fêmu/gi) || []
+            ).length;
+            const corpoInteiroTotalCount = (
+              fullText.match(/do corpo inteiro/gi) || []
+            ).length;
+            setTcCount(tcTotalCount); // Atualiza o contador de 'TC' no estado
+            setRxCount(rxTotalCount); // Atualiza o contador de 'RX' no estado
+            setLombarFemurCount(lombarFemurTotalCount); // Atualiza o contador para 'do coluna lombar e do fêmur'
+            setCorpoInteiroCount(corpoInteiroTotalCount); // Atualiza o contador para 'do corpo inteiro'
+            setPdfResults(
+              `Tomografias Realizadas: ${tcTotalCount}\nRaio-X Realizados: ${rxTotalCount}\nDesitometrias Realizadas: ${
+                lombarFemurTotalCount + corpoInteiroTotalCount
+              }\nTotal: ${
+                tcTotalCount +
+                rxTotalCount +
+                lombarFemurTotalCount +
+                corpoInteiroTotalCount
+              }`
+            );
           });
-        });
-      }, reason => {
-        console.error(reason);
-      });
+        },
+        (reason) => {
+          console.error(reason);
+        }
+      );
     };
     fileReader.readAsArrayBuffer(file);
-  };
-
-  const analyzeText = (text) => {
-    const countRX = (text.match(/ RX /g) || []).length;
-    const countTC = (text.match(/ TC /g) || []).length;
-    const countUS = (text.match(/ US /g) || []).length;
-    const countDO = (text.match(/DO CORPO INTEIRO|DO COLUNA LOMBAR E DO FÊMU/g) || []).length;
-    const resultsString = `Total RX: ${countRX}\nTotal TC: ${countTC}\nTotal US: ${countUS}\nTotal DO: ${countDO}\nTotal Exams: ${countDO + countRX + countTC + countUS}`;
-    setPdfResults(resultsString);
-    setAnalysisResults(resultsString); // Atualiza o estado local com os resultados
   };
 
   const handleDrop = (e) => {
     e.preventDefault();
     const file = e.dataTransfer.files[0];
     processPDF(file);
-    e.target.classList.remove('over');
+    e.target.classList.remove("over");
   };
 
   const handleDragOver = (e) => {
     e.preventDefault();
-    e.dataTransfer.dropEffect = 'copy';
-    e.target.classList.add('over');
+    e.dataTransfer.dropEffect = "copy";
+    e.target.classList.add("over");
   };
 
   const handleDragLeave = (e) => {
     e.preventDefault();
-    e.target.classList.remove('over');
+    e.target.classList.remove("over");
   };
 
   const handleChange = (e) => {
@@ -56,13 +93,35 @@ function PdfAnalysis({ setPdfResults }) {
   };
 
   return (
-    <div className="drop_zone" onDrop={handleDrop} onDragOver={handleDragOver} onDragLeave={handleDragLeave}>
-      <p>Arraste seu arquivo PDF aqui ou clique no botão abaixo para escolher o arquivo</p>
-      <input type="file" onChange={handleChange} accept="application/pdf" style={{ display: 'none' }} id="fileInput"/>
-      <label htmlFor="fileInput" className="button">Escolher arquivo</label>
+    <div
+      className="drop_zone"
+      onDrop={handleDrop}
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
+    >
+      <p>
+        Arraste seu arquivo PDF aqui ou clique no botão abaixo para escolher o
+        arquivo
+      </p>
+      <input
+        type="file"
+        onChange={handleChange}
+        accept="application/pdf"
+        style={{ display: "none" }}
+        id="fileInput"
+      />
+      <label htmlFor="fileInput" className="button">
+        Escolher arquivo
+      </label>
       {analysisResults && (
         <div className="analysis-results">
-          <pre>{analysisResults}</pre> {/* Exibe os resultados da análise */}
+          <p>Tomografias realizadas: {tcCount}</p>{" "}
+          {/* Exibe o contador de 'TC' */}
+          <p>Raio-x realizados: {rxCount}</p> {/* Exibe o contador de 'RX' */}
+          <p>
+            Desitometrias ralizadas: {lombarFemurCount + corpoInteiroCount}
+          </p>{" "}
+          {/* Exibe o contador de 'do coluna lombar e do fêmur' */}
         </div>
       )}
     </div>
